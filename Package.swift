54d941b600461d24fad78dc22aecb3e16ccb3009
Leslie Helou <leslie@ccs-indy.com>
2025-02-19 23:49:18 -0500

    update

--- Package.swift
+++ Package.swift
@@ -1,17 +1,24 @@
-// swift-tools-version:6.0
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "JPCredentials",
-    platforms: [.iOS(.v16), .macOS(.v13)],
+    name: "MySimpleKeychainHelper",
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "JPCredentials", targets: ["JPCredentials"])
+            name: "MySimpleKeychainHelper",
+            targets: ["MySimpleKeychainHelper"]),
     ],
     targets: [
-        .target(name: "JPCredentials", dependencies: [], path: "Sources/JPCredentials"),
-        .testTarget(name: "JPCredentialsTests", dependencies: ["JPCredentials"], path: "Tests/JPCredentialsTests")
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MySimpleKeychainHelper"),
+        .testTarget(
+            name: "MySimpleKeychainHelperTests",
+            dependencies: ["MySimpleKeychainHelper"]
+        ),
     ]
 )
