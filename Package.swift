a31cab894bdf73104c105a1978633bff22269907
Leslie Helou <leslie@ccs-indy.com>
2025-03-05 16:12:53 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,12 +17,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MySimpleKeychainHelper",
-            path: "Sources/MySimpleKeychainHelper"),
+            name: "MySimpleKeychainHelper"),
         .testTarget(
             name: "MySimpleKeychainHelperTests",
-            dependencies: ["MySimpleKeychainHelper"], 
-            path: "Tests/MySimpleKeychainHelperTests"
-        ),
+            dependencies: ["MySimpleKeychainHelper"]
+        )
     ]
 )
